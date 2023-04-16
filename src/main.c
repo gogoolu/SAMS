0b67349de2af58d670a26f32a4de7859b1ca4588
gogoolu <siuud.wu@gmail.com>
2023-04-16 13:19:23 +0800

    function all finish

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
 #define LEVEL 5
 
 int numberOfStd = 0;
@@ -19,15 +18,15 @@ Std stds[60]; // 主数组
 chinese: 1, math: 2, English: 3, total: 4, ID: 5, name: 6
 */
 
-void getHomepage();                        // draw homepage
-void getSheet();                           // get all sheet
-int inputRecord();                         // input students infor
-int getTotalAverage();                     // index ditto
-int sortBy(int subject, int mode);         // mode => descending:0, ascending:1, subject ditto
-int searchBy(int index);                   // searchBy by ID:5, name:6
-int statisticAnaly();                      // analysis statistic
-int getAll();                              // get all student infor list
-void *getMember(Std *student, int member); // get struct member int index ditto
+void getHomepage();                                                                         // draw homepage
+void getSheet(void *p, int rows, int cols, char **colheader, char **rowheader, char *type); // get all sheet
+int inputRecord();                                                                          // input students infor
+int getTotalAverage();                                                                      // index ditto
+int sortBy(int subject, int mode);                                                          // mode => descending:0, ascending:1, subject ditto
+int searchBy(int index);                                                                    // searchBy by ID:5, name:6
+int statisticAnaly();                                                                       // analysis statistic
+int getAll();                                                                               // get all student infor list
+void *getMember(Std *student, int member);                                                  // get struct member int index ditto
 int compare(void *a, void *b, int type);
 void swap(Std *a, Std *b);
 
@@ -90,6 +89,7 @@ int main()
             break;
         }
     }
+    system("pause");
 }
 
 void getHomepage()
@@ -121,13 +121,13 @@ int inputRecord()
             goto end;
         stds[numberOfStd].ID = temp;
         printf("Enter name:\n");
-        scanf("%s", &stds[numberOfStd].name);
+        scanf("%s", stds[numberOfStd].name);
         printf("Enter chinese score:\n");
-        scanf("%d", &stds[numberOfStd].chinese);
+        scanf("%f", &stds[numberOfStd].chinese);
         printf("Enter math score:\n");
-        scanf("%d", &stds[numberOfStd].math);
+        scanf("%f", &stds[numberOfStd].math);
         printf("Enter English score:\n");
-        scanf("%d", &stds[numberOfStd].english);
+        scanf("%f", &stds[numberOfStd].english);
         stds[numberOfStd].total = stds[numberOfStd].chinese + stds[numberOfStd].math + stds[numberOfStd].english;
         numberOfStd++;
     }
@@ -143,18 +143,18 @@ int getTotalAverage()
         average
     };
     system("cls");
-    int subject = 0;
-    int mode = 0;
-    printf("get CH:1,MA:2,EN:3,Total:4,ID:5\n");
-    scanf("%d", &subject);
-    printf("total:0, average:1\n");
-    scanf("%d", &mode);
-    float score = 0;
-    for (int i = 0; i < numberOfStd; ++i)
-        score += *(float *)getMember(&stds[i], subject);
-    if (mode == total)
-        score /= numberOfStd;
-    getSheet();
+    float score[2][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}};
+    // float score[2][5] = {0};
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < numberOfStd; ++j)
+            score[0][i] = *(float *)getMember(&stds[j], i + 1);
+        score[1][i] = score[0][i] / numberOfStd;
+    }
+
+    char *colheader[] = {"chinese", "math", "english", "total"};
+    char *rowheader[] = {"total", "average"};
+    getSheet(score, 2, 4, colheader, rowheader, "float");
     return 0;
 }
 
@@ -164,6 +164,10 @@ int sortBy(int subject, int mode)
         for (int j = 0; j < numberOfStd - i - 1; ++j)
             if (compare(getMember(&stds[j], mode), getMember(&stds[j + 1], mode), mode) > 0)
                 swap(&stds[j], &stds[j + 1]);
+    char *colheader[] = {"ID", "name", "chinese", "math", "english", "total"};
+    char *rowheader[] = {"0"};
+    getSheet(stds, numberOfStd, 6, colheader, rowheader, "struct");
+    return 0;
 }
 
 int searchBy(int index)
@@ -173,47 +177,58 @@ int searchBy(int index)
         ID = 5,
         NAME
     };
-    Std *result;
-    void *query_p = NULL;
-    printf("Enter the name or ID you want to query:\n");
+    Std *result = (Std *)malloc(sizeof(Std));
+    void *query_p = calloc(25, sizeof(char));
+
     if (index == ID)
-        scanf("%d", query_p);
+    {
+        printf("Enter the ID you want to query:\n");
+        scanf("%d", (int *)query_p);
+    }
     else if (index == NAME)
-        scanf("%s", query_p);
+    {
+        printf("Enter the name you want to query:\n");
+        scanf("%s", (char *)query_p);
+    }
     for (int i = 0; i < numberOfStd; ++i)
         if (!compare(getMember(&stds[i], index), query_p, index))
         {
-            result = &stds[i];
+            result[0] = stds[i];
             break;
         }
-    getSheet();
+    free(query_p);
+    char *colheader[] = {"ID", "name", "chinese", "math", "english", "total"};
+    char *rowheader[] = {"0"};
+    getSheet(result, 1, 6, colheader, rowheader, "struct");
+    return 0;
 }
 
 int statisticAnaly()
 {
-    int statistic[LEVEL] = {0};
+    int statistic[2][LEVEL] = {0};
     for (int i = 0; i < numberOfStd; ++i)
         if (stds[i].total > 89 && stds[i].total < 101)
-            statistic[0]++;
+            statistic[0][0]++;
         else if (stds[i].total > 79 && stds[i].total < 90)
-            statistic[1]++;
+            statistic[0][1]++;
         else if (stds[i].total > 69 && stds[i].total < 80)
-            statistic[2]++;
+            statistic[0][2]++;
         else if (stds[i].total > 59 && stds[i].total < 70)
-            statistic[3]++;
+            statistic[0][3]++;
         else if (stds[i].total < 60)
-            statistic[4]++;
-    getSheet();
+            statistic[0][4]++;
+    char *colheader[] = {"excellent", "good", "fair", "pass", "fail"};
+    char *rowheader[] = {"number", "ratio"};
+    getSheet(statistic, 2, 5, colheader, rowheader, "int");
+    return 0;
 }
 
 int getAll()
 {
-    getSheet();
-}
-
-void getSheet()
-{
-    ;
+    char *colheader[] = {"ID", "name", "chinese", "math", "english", "total"};
+    char *rowheader[] = {"0"};
+    getSheet(stds, numberOfStd, 6, colheader, rowheader, "struct");
+    return 0;
 }
 
 int compare(void *a, void *b, int type)
@@ -246,4 +261,66 @@ void *getMember(Std *student, int member)
     if (member == 6)
         p = &(student->name);
     return p;
+}
+
+void getSheet(void *p, int rows, int cols, char **colheader, char **rowheader, char *type)
+{
+    // judge type
+    if (strcmp(type, "int") == 0)
+    {
+        int *arr = (int *)p;
+        printf("%10s", "");
+        for (int i = 0; i < cols; i++)
+            printf("%10s", colheader[i]);
+        printf("\n");
+        // print rowheader and data
+        for (int i = 0; i < rows; i++)
+        {
+            printf("%10s", rowheader[i]);
+            for (int j = 0; j < cols; j++)
+                printf("%10d", arr[i * cols + j]);
+            printf("\n");
+        }
+    }
+    else if (strcmp(type, "float") == 0)
+    {
+        float *arr = (float *)p;
+        // print colheader
+        printf("%10s", "");
+        for (int i = 0; i < cols; i++)
+            printf("%10s", colheader[i]);
+        printf("\n");
+        for (int i = 0; i < rows; i++)
+        {
+            printf("%10s", rowheader[i]);
+            for (int j = 0; j < cols; j++)
+                printf("%10.2f", arr[i * cols + j]);
+            printf("\n");
+        }
+    }
+    else if (strcmp(type, "char") == 0)
+    {
+        char *arr = (char *)p;
+        // print colheader
+        printf("%10s", "");
+        for (int i = 0; i < cols; i++)
+            printf("%10s", colheader[i]);
+        printf("\n");
+        for (int i = 0; i < rows; i++)
+        {
+            printf("%10s", rowheader[i]);
+            for (int j = 0; j < cols; j++)
+                printf("%10c", arr[i * cols + j]);
+            printf("\n");
+        }
+    }
+    else if (strcmp("struct", type) == 0)
+    {
+        Std *arr = (Std *)p;
+        for (int i = 0; i < rows; ++i)
+            printf("%d %s %f %f %f %f ", arr[i].ID, arr[i].name, arr[i].chinese, arr[i].math, arr[i].english, arr[i].total);
+    }
+    else
+        printf("Unsupported type: %s\n", type);
+    system("pause");
 }
--- src/test.c
+++ src/test.c
@@ -1,56 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct
+typedef struct node
 {
-    float chinese;
-    float math;
-    float english;
-    float total;
-    int ID;
-    char name[25];
-} Std;
-void swap(int *a, int *b);
-int *sort(int *arr, int n);
-void *getMember(Std *student, int member);
+    int data;
+    struct node *next;
+} Node;
+
+Node *creatNode()
+{
+    Node *head = NULL;
+    return head;
+}
+
+Node *addNode(Node *head, int data)
+{
+    Node *p = head;
+    while (p != NULL)
+    {
+        p++;
+    }
+    Node *q = (Node *)malloc(sizeof(Node));
+    p->next = q;
+    q->data = data;
+    q->next = NULL;
+    return q;
+}
+
+Node *insertAfter(Node *p, int data)
+{
+    Node *q = (Node *)malloc(sizeof(Node));
+    q->data = data;
+    q->next = p->next;
+    p->next = q;
+    return q;
+}
+
+Node *getNode(Node *head, int n)
+{
+    Node *p = head;
+    int cnt = 0;
+    while (cnt != n)
+    {
+        cnt++;
+        p++;
+    }
+}
+
 int main()
 {
-    int a[] = {1, 1, 100, 99, 88, 4, 29};
-    sort(a, 7);
-    for (int i = 0; i < 7; ++i)
-        printf("%d ", a[i]);
-    return 0;
-}
-
-void *getMember(Std *student, int member)
-{
-    void *p = NULL;
-    if (member <= 4)
-        p = (float *)student + member - 1;
-    if (member == 5)
-        p = &(student->ID);
-    if (member == 6)
-        p = &(student->name);
-    return p;
-}
-
-int *sort(int *arr, int n)
-{
-    for (int i = 0; i < n - 1; ++i)
-    {
-        for (int j = 0; j < n - i - 1; ++j)
-            if (arr[j] > arr[j + 1])
-            {
-                swap(arr + j, arr + j + 1);
-            }
-    }
-    return arr;
-}
-
-void swap(int *a, int *b)
-{
-    int temp = 0;
-    temp = *a;
-    *a = *b;
-    *b = temp;
 }
